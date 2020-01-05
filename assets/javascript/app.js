49aa2aa65b0e10574b6f193e86168e2f33ae3fb4
Brandon Williams <brando5393@gmail.com>
2020-01-04 21:04:37 -0600

    starting code in place

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var category = "NONE";
+var options = [];
+var totalOptions = options.length;
+var totalSelections = 0;
+
+// display vars
+var optionDisplayText = $("#option-text-display");
+var totalOptionsDisplayText = $("#total-options-text");
+var totalSelectionsDisplayText = $("#total-selections-text");
+var categoryDisplayText = $("#category-text");
